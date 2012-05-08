50448c9b4898e4c0248ba6b390afea6c97387eeb
Kaique da Silva <kaique@main.(none)>
2012-05-08 15:59:30 -0300

    MountDB started

---
+++ mountdb.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+'''
+MountDB
+-------------------------------------------
+Convert pdf to html, 
+get content and insert in sqlite3 database.
+-------------------------------------------
+Dependencies: PDFMiner, BeautifulSoup
+'''
+
+class MountDB( object ):
+    # This class, extract and convert pdf,
+    # to html and insert the content in the
+    # datavase sqlite.
+    pass
+
+
+if __name__ == '__main__':
+    # Start docttest
+    import doctest; doctest.testmod()
