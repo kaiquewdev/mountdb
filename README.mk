344192a7e04bd92cc2ca4b1d92ca44a854eaeb8b
Kaique da Silva <kaique.developer@gmail.com>
2012-05-08 16:04:56 -0300

    README was added

---
+++ README.mk
@@ -0,0 +1,6 @@
+# MountDB
+
+MountDB is a simple, python script to mount content of html,
+text file and insert in a sqlite database.
+
+"Future support for other databases will increase. But while the sqlite file can be converted."
